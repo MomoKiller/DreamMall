2b881e287438d9cdf6a500e927d7b7026efd57fe
MomoKiller <1226424558@qq.com>
2018-12-06 20:10:23 +0800

    提交文件加修改

--- web/common/j/com_head.js
+++ web/common/j/com_head.js
@@ -2,7 +2,7 @@
  * Created by pkk on 2018/12/5.
  */
 function loadHead(){
-    var headUrl = '../resource/common/com_head.html';
+    var headUrl = '../common/com_head.html';
     $.get(headUrl, {}, function(head) {
         $('.common_head').append(head);
     });
--- web/common/j/com_tail.js
+++ web/common/j/com_tail.js
@@ -2,7 +2,7 @@
  * Created by pkk on 2018/12/5.
  */
 function loadTail(){
-    var headUrl = '../resource/common/com_tail.html';
+    var headUrl = '../common/com_tail.html';
     $.get(headUrl, {}, function(tail) {
         $('.common_tail').append(tail);
     });

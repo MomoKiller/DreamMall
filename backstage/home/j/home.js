6e8a0e02963b4b081de39e2e4217ab6b7a63f234
MomoKiller <1226421458@qq.com>
2018-12-07 15:41:06 +0800

    是真的秀

--- backstage/home/j/home.js
+++ backstage/home/j/home.js
@@ -4,42 +4,106 @@
 $(function(){
     var main = {
         api: {
+            /**** 单页面URL *****/
             surveyUrl: '../survey/survey.html',
-            productUrl: '../product/product.html',
-            customerUrl: '../customer/customer.html',
-            orderUrl: '../order/order.html',
-            dataUrl: '../data/data.html',
-            settingUrl: '../setting/setting.html'
+            productManageUrl: '../product/product_manage.html',
+            productClassUrl: '../product/product_class.html',
+            productDeliveryUrl: '../product/product_delivery.html',
+            productGiveUrl: '../product/product_give.html',
+            customerManageUrl: '../customer/customer_manage.html',
+            customerOrganizeUrl: '../customer/customer_organize.html',
+            orderAllUrl: '../order/order_all.html',
+            orderRecordUrl: '../order/order_record.html',
+            dataFlowUrl: '../data/data_flow.html',
+            dataExchangeUrl: '../data/data_exchange.html',
+            dataProductUrl: '../data/data_product.html',
+            dataSaleUrl: '../data/data_sale.html',
+            dataIncomeUrl: '../data/data_income.html',
+            settingStaffUrl: '../setting/setting_staff.html',
+            settingBannerUrl: '../setting/setting_banner.html',
+            settingMsgUrl: '../setting/setting_msg.html',
+            settingOrderUrl: '../setting/setting_order.html',
+            settingInfoUrl: '../setting/setting_info.html',
+            settingReminderUrl: '../setting/setting_reminder.html',
+            settingDialogUrl: '../setting/setting_dialog.html'
+            
         },
         init: function(){
             main.loadComHead();
             main.loadComNav();
-            main.loadMain();        
+            main.loadMain(0, 0);      
         },
         // 加载主页
-        loadMain: function(navIndex){
-            if(!navIndex){                  // 首次加载概况
-                navIndex = 0;
-            }
-            var mainUrlKey = {
-                0: 'surveyUrl',
-                1: 'productUrl',
-                2: 'customerUrl',
-                3: 'orderUrl',
-                4: 'dataUrl',
-                5: 'settingUrl'
-            };
-            $.get(main.api[mainUrlKey[navIndex]], {}, function(mainHtml){
+        loadMain: function(index1, index2){
+            var mainUrlKey = [{
+                0: 'surveyUrl'
+            },{
+                0: 'productManageUrl',
+                1: 'productClassUrl',
+                2: 'productDeliveryUrl',
+                3: 'productGiveUrl'
+            },{
+                0: 'customerManageUrl',
+                1: 'customerOrganizeUrl'
+            },{
+                0: 'orderAllUrl',
+                1: 'orderRecordUrl'
+            },{
+                0: 'dataFlowUrl',
+                1: 'dataExchangeUrl',
+                2: 'dataProductUrl',
+                3: 'dataSaleUrl',
+                4: 'dataIncomeUrl'
+            },{
+                0: 'settingStaffUrl',
+                1: 'settingBannerUrl',
+                2: 'settingMsgUrl',
+                3: 'settingOrderUrl',
+                4: 'settingInfoUrl',
+                5: 'settingReminderUrl',
+                6: 'settingDialogUrl'
+            }];
+
+            $.get(main.api[mainUrlKey[index1][index2]], {}, function(mainHtml){
                 $('.common_main').html(mainHtml);
             });
         },
         // 左侧导航
         loadComNav: function(){
-            var domNavLi = $('.common_nav li');
-            domNavLi.bind('click', function(){
-                var navIndex = $(this).index();
-                $(this).addClass('active').siblings().removeClass('active');
-                main.loadMain(navIndex);
+            var domLi = $('.common_nav>ul>li>a');
+            var domLiSec = $('.common_nav li .level_2 a');
+            var index1 = 0;
+            var index2 = 0;
+            // 一级菜单事件
+            domLi.bind('click', function(){
+                var _this = $(this).parent('li');
+                index1 = _this.index();
+                if(_this.find('.level_2').length > 0){  // 有二级菜单
+                    $('.common_nav>ul>li').removeClass('active');
+                    if(_this.find('.level_2').is(':hidden')){       //展示
+                        _this.find('.level_2').css('display', 'block');
+                        $(this).find('i').addClass('glyphicon-triangle-bottom').removeClass('glyphicon-triangle-right');
+                        // 展示二级菜单的第一个
+                        domLiSec.removeClass('active');
+                        _this.find('.level_2 a').eq(0).addClass('active');
+                        main.loadMain(index1, index2);
+                    }else{
+                        _this.find('.level_2').css('display', 'none');
+                        $(this).find('i').addClass('glyphicon-triangle-right').removeClass('glyphicon-triangle-bottom');
+                    }
+                }else{      // 一级菜单
+                    domLiSec.removeClass('active');
+                    _this.addClass('active').siblings().removeClass('active');
+                    main.loadMain(index1, index2);
+                }
+            });
+            // 二级菜单事件
+            domLiSec.bind('click', function(){
+                var _this = $(this);
+                $('.common_nav>ul>li').removeClass('active');
+                domLiSec.removeClass('active');
+                _this.addClass('active').siblings().removeClass('active');
+                main.loadMain(_this.parents('li').index(), _this.index());
             });
         },
         // 头部信息
--- backstage/login/j/login.js
+++ backstage/login/j/login.js
@@ -1,3 +1,20 @@
 /**
  * Created by pkk on 2018/12/6.
  */
+$(function(){
+    var login = {
+        api: {
+            loginUrl: ''
+        },
+        init: function(){
+            login.bindEvent();
+        },
+        bindEvent: function(){
+            var domLoginBtn = $('.login_form .login_btn');
+            domLoginBtn.bind('click', function(){
+                location.href = '../home/home.html';
+            });
+        }
+    };
+    login.init();
+});

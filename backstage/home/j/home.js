2e327be64533ea5bad18331f0ee810fdb9c9aa13
MomoKiller <1226421458@qq.com>
2018-12-07 12:12:29 +0800

    陈独秀的秀

--- backstage/home/j/home.js
+++ backstage/home/j/home.js
@@ -1,5 +1,51 @@
 /**
  * Created by pkk on 2018/12/6.
  */
-// 概况
-loadSurvey();
+$(function(){
+    var main = {
+        api: {
+            surveyUrl: '../survey/survey.html',
+            productUrl: '../product/product.html',
+            customerUrl: '../customer/customer.html',
+            orderUrl: '../order/order.html',
+            dataUrl: '../data/data.html',
+            settingUrl: '../setting/setting.html'
+        },
+        init: function(){
+            main.loadComHead();
+            main.loadComNav();
+            main.loadMain();        
+        },
+        // 加载主页
+        loadMain: function(navIndex){
+            if(!navIndex){                  // 首次加载概况
+                navIndex = 0;
+            }
+            var mainUrlKey = {
+                0: 'surveyUrl',
+                1: 'productUrl',
+                2: 'customerUrl',
+                3: 'orderUrl',
+                4: 'dataUrl',
+                5: 'settingUrl'
+            };
+            $.get(main.api[mainUrlKey[navIndex]], {}, function(mainHtml){
+                $('.common_main').html(mainHtml);
+            });
+        },
+        // 左侧导航
+        loadComNav: function(){
+            var domNavLi = $('.common_nav li');
+            domNavLi.bind('click', function(){
+                var navIndex = $(this).index();
+                $(this).addClass('active').siblings().removeClass('active');
+                main.loadMain(navIndex);
+            });
+        },
+        // 头部信息
+        loadComHead: function(){
+
+        }
+    };
+    main.init();
+});
